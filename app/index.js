74658c0919160b09a4923b463edbbe503ff22c76
Nguyen Duong <nduongg04@gmail.com>
2024-03-16 01:25:32 +0700

    Add Nativewind library

--- app/index.js
+++ app/index.js
@@ -7,27 +7,10 @@ import { StatusBar } from "expo-status-bar";
 
 const Home = () => {
 	return (
-		<View className="flex-1 items-center justify-center bg-blue-500">
-			<Text>Open up App.js to start working on your app!</Text>
+		<View className="flex-1 justify-center items-center bg-blue-700 ">
+			<Text className="text-white">Open up App.js to start working on your app!</Text>
 			<StatusBar style="auto" />
 		</View>
-	);
-};
-
-export default Home;
-
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native";
-import favicon from "@assets/favicon.png";
-import { Image } from "react-native";
-import { ScreenHeaderBtn } from "@components/common";
-
-const Home = () => {
-	return (
-		<SafeAreaView>
-			<Text>Hello</Text>
-            <Image uri={favicon}/>
-		</SafeAreaView>
 	);
 };
 
