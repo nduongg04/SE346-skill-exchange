936ff0f19b5a9cda29ece07f385acc8a4c9f89f6
Tran Thanh Dat <neban0444@gmail.com>
2024-03-15 14:52:28 +0700

    welcome screen

--- components/register/EnterName.js
+++ components/register/EnterName.js
@@ -1,9 +1,14 @@
 import { registerRootComponent } from 'expo';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import {Dimensions} from 'react-native';
+import { useFonts } from 'expo-font';
+import {COLORS} from '../../constants'
 
 function App() {
+    const [fontsLoaded, fontError] = useFonts({
+        'Coda-Regular': require('../../assets/fonts/Coda-Regular.ttf'),
+      });
     return (
         <LinearGradient
             style={styles.container}
@@ -16,15 +21,75 @@ function App() {
                     />
                 <Text
                     style={{
-                        fontSize: 21, 
-                        color: '#FF843C',
-                        margin: 20,
+                        fontSize: 19, 
+                        color: COLORS.orange,
+                        marginTop: 15,
+                        marginBottom: 15,
                         alignSelf: 'center',
                         fontFamily: 'Coda-Regular'      
                     }}
                     >
-                    Welcome
+                    Welcomee
+                </Text >
+                <Text style={styles.text_center}>
+                    LEARN a new skill                   
                 </Text>
+                <Text style={styles.text_center}>
+                    GET a new friend
+                </Text>
+                <View 
+                    style={{
+                        height: 4, 
+                        backgroundColor: COLORS.purple, 
+                        borderRadius: 50,
+                        width: 120,
+                        alignSelf: 'center',
+                        margin: 15 }}></View>
+                <Text
+                    style={{
+                        fontSize: 17, 
+                        color: COLORS.orange,
+                        marginBottom: 15,
+                        alignSelf: 'center',
+                        fontFamily: 'Coda-Regular'      
+                    }}
+                    >Registerr</Text >
+                <TextInput
+                    style={{height: 45, 
+                            borderRadius: 30, 
+                            width: 285,
+                            alignSelf: 'center', 
+                            borderColor: COLORS.black,
+                            borderWidth: 0.5, 
+                            backgroundColor: COLORS.lightGray,
+                            paddingLeft: 30, 
+                            fontSize: 14, 
+                            fontFamily: 'Coda-Regular'                       
+                        }}
+                    placeholder="Your name">
+                </TextInput>
+                <TouchableOpacity style={[styles.button, {backgroundColor: COLORS.orange}]}>
+                    <Text 
+                        style={[styles.buttonText,{color: COLORS.lightWhite}]}>Nextt</Text>
+                </TouchableOpacity>
+                <View style={{flex: 1, flexDirection: 'row', marginTop: 20, marginLeft: 20}}>
+                    <Text 
+                        style={{
+                            fontFamily: 'Coda-Regular',
+                            fontSize: 14, marginRight: 5, marginTop: 5                     
+                        }}>Already have an account?</Text>
+                    <TouchableOpacity 
+                        style={[{
+                            borderRadius: 25,
+                            width: 80, height: 33,
+                            justifyContent: 'center',
+                            backgroundColor: COLORS.lightWhite, 
+                            borderColor: COLORS.orange,
+                            borderWidth: 0.5}]}>
+                        <Text 
+                            style={[styles.buttonText,{color: COLORS.orange}]}>Login</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </LinearGradient>
     );
@@ -48,8 +113,30 @@ const styles = StyleSheet.create({
         paddingTop: 25
     },
     image: {
-        height: 160,
-        width: 365
+        height: windowHeight*147/896,
+        width: 305,
+        alignSelf: 'center'
+    },
+    text_center: {
+        fontSize: 17,
+        alignSelf: 'center',
+        fontFamily: 'Coda-Regular'
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'flex-end',        
+        borderRadius: 25,
+        width: 80, height: 33,
+        marginTop: 10,
+        marginRight: 35 
+    },
+    buttonText: {
+        fontSize: 14, 
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        marginTop: 0,
+        fontFamily: 'Coda-Regular'
     }
 
 });
--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,10 @@
 const COLORS = {
 	lightWhite: "#FFFFFF",
+    black: "000000",
     skyBlue: "#1A82C7",
+    orange: "#FF843C",
+    purple: "#7751C7",
+    lightGray: "#F9F9F9"
 };
 
 export { COLORS };
