442c626b36a225bc069a7b2cdc8589d445b87f17
DQP-UIT <22521149@gm.uit.edu.vn>
2024-03-28 17:42:37 +0700

    Search

--- Search_Tutorials_Tab/Data_Topic_List.jsx
+++ Search_Tutorials_Tab/Data_Topic_List.jsx
@@ -1,12 +1,12 @@
 import Category from "./Category/Category"
-import Suzy from "@assets/icons/Suzy.png"
+import Suzy from './assets/icons/Suzy.png'
 const Data = [
-    { id: '1', Category: <Category imageUri={require('./assets/icons/Suzy.png')} name="1" /> },
-    { id: '2', Category: <Category imageUri={require('./assets/icons/Suzy.png')} name="2" /> },
-    { id: '3', Category: <Category imageUri={require('./assets/icons/Suzy.png')} name="3" /> },
-    { id: '4', Category: <Category imageUri={require('./assets/icons/Suzy.png')} name="4" /> },
-    { id: '5', Category: <Category imageUri={require('./assets/icons/Suzy.png')} name="5" /> },
-    { id: '6', Category: <Category imageUri={require('./assets/icons/Suzy.png')} name="6" /> },
+    { id: '1', Category: <Category imageUri={Suzy} name="1" /> },
+    { id: '2', Category: <Category imageUri={Suzy} name="2" /> },
+    { id: '3', Category: <Category imageUri={Suzy} name="3" /> },
+    { id: '4', Category: <Category imageUri={Suzy} name="4" /> },
+    { id: '5', Category: <Category imageUri={Suzy} name="5" /> },
+    { id: '6', Category: <Category imageUri={Suzy} name="6" /> },
 ]
 
 export default Data;
