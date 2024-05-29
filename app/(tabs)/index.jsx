d6c52d171db1ce60287e8d5ecf22ff0f36d7bed9
DuongWuangDat <duongquangdat172004@gmail.com>
2024-05-29 10:23:40 +0700

    fix

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -3,21 +3,10 @@ import { useSession } from "../../context/AuthContext";
 import GetData from "../../utils/getdata";
 
 export default function Index() {
-<<<<<<< HEAD
-    const {user} = useSession();
-    if (user) {
-        return <Redirect href="/home" />;
-    } else {
-        return <Redirect href="/signing/Signing" />;
-    }
-   // return <Redirect href="/home"/>;
-    
-=======
 	const { user } = useSession();
 	if (user) {
 		return <Redirect href="/home" />;
 	} else {
 		return <Redirect href="/signing/Signing" />;
 	}
->>>>>>> ff9e2483edb1741ce3d280c4419d798adc04fc3f
 }
