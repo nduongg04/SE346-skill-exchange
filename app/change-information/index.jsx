5ccd1cae82ac93163f985827afa7c72d3f3cbc91
Nguyen Duong <nduongg04@gmail.com>
2024-06-21 02:23:41 +0700

    Fix error fill all change information

--- app/change-information/index.jsx
+++ app/change-information/index.jsx
@@ -1,12 +1,12 @@
 import {
-    Text,
-    View,
-    TouchableOpacity,
-    SafeAreaView,
-    Alert,
-    ScrollView,
-    TouchableWithoutFeedback,
-    Keyboard
+	Text,
+	View,
+	TouchableOpacity,
+	SafeAreaView,
+	Alert,
+	ScrollView,
+	TouchableWithoutFeedback,
+	Keyboard,
 } from "react-native";
 import styles from "../../components/register/style";
 import InputText from "../../components/register/Button/InputText";
@@ -25,160 +25,174 @@ import PatchData from "../../utils/patchdata";
 import avatarDefault from "@assets/images/avatarDefault.jpg";
 
 const ChangeInformation = () => {
-    const {name}= useLocalSearchParams()
-    const {mail} =useLocalSearchParams()
-    const {number} = useLocalSearchParams()
-    const [skill, setSkill] = useState("");
-    const { user, login } = useSession();
-    const [isUpdating, setIsUpdating] = useState(false);
-    const [username, setUserName] = useState(name)
-    const [email, setEmail] = useState(mail)
-    const [phonenumber,setPhoneNumber] = useState(number)
-    const handleChangeInformation = async () => {
-        setIsUpdating(true);
-        const baseUrl = "https://se346-skillexchangebe.onrender.com";
-        const data = await PatchData(`${baseUrl}/api/v1/user/update/${user.id}`, {
-            username: username,
-            email: email,
-            phoneNumber: phonenumber
-        });
-        if (!data || data === "404" || data === "Something went wrong") {
-            alert("Something went wrong when updating user's skill");
-            setIsUpdating(false);
-            return;
-        }
-        login({
-            ...user,
-            username: username,
-            email: email,
-            phoneNumber: phonenumber
-        });
-        Alert.alert("Successfully", "Update successfully", [
-            {
-                text: "OK",
-                onPress: () => {
-                    router.replace("/profile");
-                },
-            },
-        ]);
-        setIsUpdating(false);
-    };
-    
-    return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <SafeAreaView
-            style={{
-                flex: 1,
-            }}
-        >
-            <Stack.Screen
-                options={{
-                    title: "Change skill description",
-                    headerShown: false,
-                    headerTitle: "",
-                }}
-            />
-            <Spinner
-                visible={isUpdating}
-                textContent={"Updating..."}
-                textStyle={{ color: "#FFF" }}
-            />
-            <LinearGradient
-                style={{
-                    flex: 1,
-                    backgroundColor: "#fff",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-                colors={["#FFBE98", "#7751C7"]}
-            >
-                <View
-                    style={{
-                        backgroundColor: "#fff",
-                        borderRadius: 30,
-                        height: "auto", //554/896
-                        width: scale(320), //372/410,
-                        padding: 20,
-                    }}
-                >
-                    <TouchableOpacity
-                        onPress={() => {
-                            router.back();
-                        }}
-                        style={{
-                            flexDirection: "row",
-                            alignItems: "center",
-                        }}
-                    >
-                        <AntDesign
-                            name="arrowleft"
-                            size={16}
-                            color={COLORS.orange}
-                            style={{ marginRight: 5 }}
-                        />
-                        <Text
-                            style={{
-                                fontSize: 14,
-                                fontFamily: "CodaRegular",
-                                color: COLORS.orange,
-                            }}
-                        >
-                            Back
-                        </Text>
-                    </TouchableOpacity>
-                    
-                    <Text style={[styles.text_center, { marginTop: 10 }]}>
-                        CHANGE INFORMATION
-                    </Text>
-                    <InputText
-                        placeholder='Your username'
-                        label='Username'
-                        error={null}
-                        value= {username}
-                        onChangeText= {(text)=>{setUserName(text)}}
-                    />
-                    <InputText
-                        placeholder='Your email address'
-                        label='Email'
-                        error={null} 
-                        value={email}
-                        onChangeText={(text)=> {setEmail(text)}}
-                    />
-                    <InputText
-                        placeholder='Your phone number'
-                        label='Phone Number'
-                        error={null}
-                        keyboardType='numeric'
-                        value={phonenumber}
-                        onChangeText={(text)=>{setPhoneNumber(text)}}
-                    />
+	const { name } = useLocalSearchParams();
+	const { mail } = useLocalSearchParams();
+	const { number } = useLocalSearchParams();
+	const [skill, setSkill] = useState("");
+	const { user, login } = useSession();
+	const [isUpdating, setIsUpdating] = useState(false);
+	const [username, setUserName] = useState(name);
+	const [email, setEmail] = useState(mail);
+	const [phonenumber, setPhoneNumber] = useState(number);
+	const handleChangeInformation = async () => {
+		if (username === "" || email === "" || phonenumber === "") {
+			Alert.alert("Error", "Please fill all the fields", [
+				{
+					text: "OK",
+				},
+			]);
+			return;
+		}
+		setIsUpdating(true);
+		const baseUrl = "https://se346-skillexchangebe.onrender.com";
+		const data = await PatchData(`${baseUrl}/api/v1/user/update/${user.id}`, {
+			username: username,
+			email: email,
+			phoneNumber: phonenumber,
+		});
+		if (!data || data === "404" || data === "Something went wrong") {
+			alert("Something went wrong when updating user's skill");
+			setIsUpdating(false);
+			return;
+		}
+		login({
+			...user,
+			username: username,
+			email: email,
+			phoneNumber: phonenumber,
+		});
+		Alert.alert("Successfully", "Update successfully", [
+			{
+				text: "OK",
+				onPress: () => {
+					router.replace("/profile");
+				},
+			},
+		]);
+		setIsUpdating(false);
+	};
 
-                    <TouchableOpacity
-                        style={{
-                            backgroundColor: COLORS.orange,
-                            borderRadius: 27,
-                            alignSelf: "flex-end",
-                            paddingHorizontal: 20,
-                            paddingVertical: 7,
-                            marginTop: 16,
-                        }}
-                        onPress={handleChangeInformation}
-                    >
-                        <Text
-                            style={{
-                                textAlign: "center",
-                                fontSize: 16,
-                                color: COLORS.white,
-                            }}
-                        >
-                            Change
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </LinearGradient>
-        </SafeAreaView>
-        </TouchableWithoutFeedback>
-    );
+	return (
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<SafeAreaView
+				style={{
+					flex: 1,
+				}}
+			>
+				<Stack.Screen
+					options={{
+						title: "Change skill description",
+						headerShown: false,
+						headerTitle: "",
+					}}
+				/>
+				<Spinner
+					visible={isUpdating}
+					textContent={"Updating..."}
+					textStyle={{ color: "#FFF" }}
+				/>
+				<LinearGradient
+					style={{
+						flex: 1,
+						backgroundColor: "#fff",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+					colors={["#FFBE98", "#7751C7"]}
+				>
+					<View
+						style={{
+							backgroundColor: "#fff",
+							borderRadius: 30,
+							height: "auto", //554/896
+							width: scale(320), //372/410,
+							padding: 20,
+						}}
+					>
+						<TouchableOpacity
+							onPress={() => {
+								router.back();
+							}}
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+							}}
+						>
+							<AntDesign
+								name="arrowleft"
+								size={16}
+								color={COLORS.orange}
+								style={{ marginRight: 5 }}
+							/>
+							<Text
+								style={{
+									fontSize: 14,
+									fontFamily: "CodaRegular",
+									color: COLORS.orange,
+								}}
+							>
+								Back
+							</Text>
+						</TouchableOpacity>
+
+						<Text style={[styles.text_center, { marginTop: 10 }]}>
+							CHANGE INFORMATION
+						</Text>
+						<InputText
+							placeholder="Your username"
+							label="Username"
+							error={null}
+							value={username}
+							onChangeText={(text) => {
+								setUserName(text);
+							}}
+						/>
+						<InputText
+							placeholder="Your email address"
+							label="Email"
+							error={null}
+							value={email}
+							onChangeText={(text) => {
+								setEmail(text);
+							}}
+						/>
+						<InputText
+							placeholder="Your phone number"
+							label="Phone Number"
+							error={null}
+							keyboardType="numeric"
+							value={phonenumber}
+							onChangeText={(text) => {
+								setPhoneNumber(text);
+							}}
+						/>
+
+						<TouchableOpacity
+							style={{
+								backgroundColor: COLORS.orange,
+								borderRadius: 27,
+								alignSelf: "flex-end",
+								paddingHorizontal: 20,
+								paddingVertical: 7,
+								marginTop: 16,
+							}}
+							onPress={handleChangeInformation}
+						>
+							<Text
+								style={{
+									textAlign: "center",
+									fontSize: 16,
+									color: COLORS.white,
+								}}
+							>
+								Change
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</LinearGradient>
+			</SafeAreaView>
+		</TouchableWithoutFeedback>
+	);
 };
 
 export default ChangeInformation;
